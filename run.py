82ae4262fe46474aa860ace3cc6e49984331bd0a
brettalanmeyer <brettalanmeyer@gmail.com>
2017-03-21 12:59:38 -0500

    Logging request errors and moved code into main()

--- run.py
+++ run.py
@@ -14,21 +14,6 @@ app.config.from_pyfile("config.cfg")
 assets = Environment(app)
 socketio = SocketIO(app)
 
-engine = create_engine("mysql+mysqldb://" + app.config["MYSQL_USERNAME"] + ":" + app.config["MYSQL_PASSWORD"] + "@" + app.config["MYSQL_HOST"] + "/" + app.config["MYSQL_DATABASE"], pool_recycle = 3600)
-db_session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
-Session = sessionmaker(bind = engine)
-session = Session()
-
-ismService = IsmService.IsmService(session)
-playerService = PlayerService.PlayerService(session)
-matchService = MatchService.MatchService(session)
-scoreService = ScoreService.ScoreService(session)
-leaderboardService = LeaderboardService.LeaderboardService(session)
-
-singles = Singles.Singles(session)
-doubles = Doubles.Doubles(session)
-nines = Nines.Nines(session)
-
 @app.route("/", methods = ["GET"])
 def index():
 	return render_template("main/index.html")
@@ -207,7 +192,6 @@ def buttons_score(button):
 		data = matchType.score(match, button)
 	else:
 		latestMatch = matchService.selectLatestMatch()
-		print(latestMatch.id)
 		matchType = getMatchType(latestMatch)
 		newMatch = matchType.playAgain(latestMatch)
 		data = {
@@ -246,10 +230,14 @@ def afterRequest(response):
 
 @app.errorhandler(404)
 def not_found(error):
+	app.logger.error(error)
+	app.logger.error(request.url)
 	return render_template("errors/404.html"), 404
 
 @app.errorhandler(500)
 def server_error(error):
+	app.logger.error(error)
+	app.logger.error(request.url)
 	return render_template("errors/500.html"), 500
 
 def getMatchType(match):
@@ -262,11 +250,31 @@ def getMatchType(match):
 	elif nines.isMatchType(match.matchType):
 		return nines
 
-if __name__ == "__main__":
+def setupLogging():
 	handler = TimedRotatingFileHandler(app.config["LOG_FILE"], when = app.config["LOG_WHEN"], interval = app.config["LOG_INTERVAL"], backupCount = app.config["LOG_BACKUP_COUNT"])
 	handler.setLevel(logging.INFO)
 	formatter = logging.Formatter(app.config["LOG_FORMAT"])
 	handler.setFormatter(formatter)
 	app.logger.addHandler(handler)
+
+def setupSession():
+	engine = create_engine("mysql+mysqldb://" + app.config["MYSQL_USERNAME"] + ":" + app.config["MYSQL_PASSWORD"] + "@" + app.config["MYSQL_HOST"] + "/" + app.config["MYSQL_DATABASE"], pool_recycle = 3600)
+	db_session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
+	Session = sessionmaker(bind = engine)
+	return Session()
+
+if __name__ == "__main__":
+	setupLogging()
+	session = setupSession()
+
+	ismService = IsmService.IsmService(session)
+	playerService = PlayerService.PlayerService(session)
+	matchService = MatchService.MatchService(session)
+	scoreService = ScoreService.ScoreService(session)
+	leaderboardService = LeaderboardService.LeaderboardService(session)
+
+	singles = Singles.Singles(session)
+	doubles = Doubles.Doubles(session)
+	nines = Nines.Nines(session)
 
 	socketio.run(app, host = app.config["HOST"], port = app.config["PORT"], debug = app.config["DEBUG"])
