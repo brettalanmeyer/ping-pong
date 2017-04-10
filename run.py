beab848fb1738e9106b412dae7ac83bba8783e28
brettalanmeyer <brettalanmeyer@gmail.com>
2017-04-10 10:15:41 -0500

    Removed audio test route.

--- run.py
+++ run.py
@@ -283,10 +283,6 @@ def getMatchType(match):
 	elif nines.isMatchType(match.matchType):
 		return nines
 
-@app.route("/audio-test", methods = ["GET"])
-def audioTest():
-	return render_template("audio-test.html")
-
 if __name__ == "__main__":
 	logger.setupLogging(app)
 	assets.setupAssets(app)
