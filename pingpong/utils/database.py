524922228c02846e84091bc9f6ce50b83162de78
brettalanmeyer <brettalanmeyer@gmail.com>
2017-04-13 16:49:47 -0500

    Fix

--- pingpong/utils/database.py
+++ pingpong/utils/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("mysql+mysqldb://brett:r45ftthry@localhost/ping-pong", pool_recycle = 3600)
+engine = create_engine("mysql+mysqldb://darts:KVUgJnMfAu26FtR4@localhost/pingpong", pool_recycle = 3600)
 session = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
