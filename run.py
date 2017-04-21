a3a5a0fbf45903cfd182c92c6c1dca70a5b53a50
brettalanmeyer <brettalanmeyer@gmail.com>
2017-04-21 10:24:43 -0500

    Fixed for unicode/utf8 encoding issues.

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from pingpong import create_app, socketio
+import sys
 
 app = create_app(debug = True)
 
+def setEncoding():
+	reload(sys)
+	sys.setdefaultencoding("UTF8")
+
 if __name__ == "__main__":
+	setEncoding()
 	socketio.run(app, host = app.config["HOST"], port = app.config["PORT"], debug = app.config["DEBUG"])
