7f689af666f29e7d1a717b86c4d5dca567fdab34
brettalanmeyer <brettalanmeyer@gmail.com>
2017-04-25 13:50:16 -0500

    Notifications for new players and when they update their name.

--- pingpong/controllers/PlayerController.py
+++ pingpong/controllers/PlayerController.py
@@ -6,6 +6,7 @@ from flask import request
 from flask import Response
 from pingpong.forms.PlayerForm import PlayerForm
 from pingpong.services.PlayerService import PlayerService
+from pingpong.utils import notifications
 
 playerController = Blueprint("playerController", __name__)
 
@@ -32,7 +33,10 @@ def players_create(matchId):
 		playerForm.load(player, request.form)
 		return render_template("players/new.html", player = player, matchId = matchId), 400
 	else:
-		playerService.create(request.form)
+		player = playerService.create(request.form)
+
+		message = "<b>{}</b> has joined ping pong. Please consider adding them to the ping pong chat group.".format(player.name)
+		notifications.send(message)
 
 		if matchId != None:
 			return redirect("/matches/%d/players" % matchId)
@@ -53,11 +57,21 @@ def players_edit(id):
 def players_update(id):
 	hasErrors = playerForm.validate(id, request.form)
 
+	player = playerService.selectById(id)
+	originalName = player.name
+
 	if hasErrors:
-		player = playerService.selectById(id)
 		playerForm.load(player, request.form)
 		return render_template("players/edit.html", player = player), 400
+
 	else:
-		player = playerService.update(id, request.form)
-		flash("Player '{}' has been successfully updated.".format(player.name), "success")
+		updatedPlayer = playerService.update(id, request.form)
+		newName = updatedPlayer.name
+
+		flash("Player '{}' has been successfully updated.".format(updatedPlayer.name), "success")
+
+		if originalName != newName:
+			message = "<b>{}</b> is now known as <b>{}</b>.".format(originalName, newName)
+			notifications.send(message)
+
 		return redirect("/players")
