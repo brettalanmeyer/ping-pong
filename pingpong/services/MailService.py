c537840cb280a06b09abbe0d2ccea81f771cbd59
brettalanmeyer <brettalanmeyer@gmail.com>
2017-06-02 15:01:27 -0500

    Logged email messages.

--- pingpong/services/MailService.py
+++ pingpong/services/MailService.py
@@ -5,6 +5,8 @@ from flask import current_app as app
 class MailService():
 
 	def sendFeedback(self, name, email, message):
+		app.logger.info("Sending Feedback...")
+
 		subject = "Ping Pong App Feedback"
 		body = "Ping Pong App Feedback\nName: {}\nEmail: {}\n\n{}".format(name, email, message)
 		html = "<h2>Ping Pong App Feedback</h2><p>Name: {}<br />Email: {}<p><p>{}</p>".format(name, email, message)
@@ -14,8 +16,19 @@ class MailService():
 		sender = (app.config["MAIL_FROM_NAME"], app.config["MAIL_FROM_EMAIL"])
 		recipients = app.config["MAIL_RECIPIENTS"]
 
+		app.logger.info("\
+			Sending Mail...\n\
+			FROM: %s\n\
+			TO: %s\n\
+			SUBJECT: %s\n\
+			BODY: %s\n\
+			HTML: %s\
+		", sender, recipients, subject, body, html)
+
 		mail = Mail(app)
 		messages = Message(subject, sender = sender, recipients = recipients)
 		messages.body = body
 		messages.html = html
 		mail.send(messages)
+
+		app.logger.info("Mail Sent")
