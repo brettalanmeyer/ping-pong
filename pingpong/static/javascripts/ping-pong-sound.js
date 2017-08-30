0044047902a492a9134f4276fb333a590dcba18b
brettalanmeyer <brettalanmeyer@gmail.com>
2017-08-30 13:35:32 -0500

    Shuffle audio files then playing through them in that order.

--- pingpong/static/javascripts/ping-pong-sound.js
+++ pingpong/static/javascripts/ping-pong-sound.js
@@ -1,12 +1,14 @@
 function PingPongSound(){
 
 	this.audioFiles = [];
+	this.index = 0;
 
 	for(var i in arguments){
 		var audio = new Audio("/static/audio/" + arguments[i]);
 		audio.load();
 		this.audioFiles.push(audio);
 	}
+	shuffle(this.audioFiles);
 
 	this.play = function(playSound){
 		if(playSound == undefined){
@@ -14,10 +16,11 @@ function PingPongSound(){
 		}
 
 		if(playSound && this.audioFiles.length > 0){
-			var index = randRange(0, this.audioFiles.length - 1);
-			var audio = this.audioFiles[index];
+			var audio = this.audioFiles[this.index % this.audioFiles.length];
 			audio.currentTime = 0;
 			audio.play();
+			this.index++;
 		}
 	}
 }
+
